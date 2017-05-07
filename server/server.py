12f647e9f47807147398696f45ae639c6d6eeb96
arcelan.sara <lu_zhiyuan@yeah.net>
2017-05-07 21:01:08 +0800

    modify server.py for resolve test.

--- server/server.py
+++ server/server.py
@@ -7,7 +7,7 @@ urls = (
 
 class index:
     def GET(self):
-        return "get Hello, arcelan world xxx"
+        return "get Hello, arcelan world, wow, great!"
     def POST(self):
         """ Add new entry """
         return "post Hello, world"
