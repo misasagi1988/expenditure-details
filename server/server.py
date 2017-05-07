5373def9ea5058f5043c6e804cbc64b5207e1ef3
孟玉静 <meng_yujing@yeah.net>
2017-05-07 19:51:31 +0800

    init commit
    add server

---
+++ server/server.py
@@ -0,0 +1,42 @@
+import web
+
+urls = (
+    '/', 'index',
+    '/blog', 'blog'
+)
+
+class index:
+    def GET(self):
+        return "get Hello, world!"
+
+    def POST(self):
+        """ Add new entry """
+        return "post Hello, world!"
+
+    def PUT(self):
+        """ Add new entry """
+        return "put Hello, world!"
+
+    def DELETE(self):
+        """ Add new entry """
+        return "delete Hello, world!"
+
+class blog:
+    def GET(self):
+        return "get blog Hello, world!"
+
+    def POST(self):
+        """ Add new entry """
+        return "post blog Hello, world!"
+
+    def PUT(self):
+        """ Add new entry """
+        return "put blog Hello, world!"
+
+    def DELETE(self):
+        """ Add new entry """
+        return "delete blog Hello, world!"
+
+if __name__ == "__main__":
+    app = web.application(urls, globals())
+    app.run()
